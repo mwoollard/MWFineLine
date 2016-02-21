5966127ff6e293feb65ecf36c9ee16d03ddb5f77
Mark Woollard <mwoollard@newforma.com>
2016-02-21 13:09:35 +0000

    Initial Commit

---
+++ MWFineLine/MWFineLine.h
@@ -0,0 +1,19 @@
+//
+//  MWFineLine.h
+//  MWFineLine
+//
+//  Created by Mark Woollard on 21/02/2016.
+//  Copyright © 2016 Mark Woollard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWFineLine.
+FOUNDATION_EXPORT double MWFineLineVersionNumber;
+
+//! Project version string for MWFineLine.
+FOUNDATION_EXPORT const unsigned char MWFineLineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWFineLine/PublicHeader.h>
+
+
